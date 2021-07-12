ba92f0d05fb62b95d5920d44596226006eac8790
tulio-fergulha <tfergulha@gmail.com>
2021-07-12 11:52:03 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+import Comments from "./components/Comments";
+
+export default {
+  name: "App",
+  components: {
+    Comments,
+  },
+};
+</script>
+
+<style></style>
